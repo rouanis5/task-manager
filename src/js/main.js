942d3108a718bbcb583d669c4254dc3c8b172cb8
rouanis5 <ouanis42@gmail.com>
2021-10-27 16:26:21 +0100

    add an alert + correct tasks order

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,25 @@ let addTask = document.getElementById("addTask");
 let inputTask = document.getElementById("inputTask");
 let taskContainer = document.querySelector(".tasks .container");
 let tasks = [];
-addTask.onclick = () =>{
-    if (inputTask.value !== ""){
+addTask.addEventListener("click",(e) =>{
+    let msg = "Please fill the chunck !S";
+    if (inputTask.value !== "" && inputTask.value !== msg){
         tasks.push(inputTask.value);
         tasker(inputTask.value)
-        // inputTask.value = "";
+        inputTask.value = "";
     }
-}
+    else{
+        e.preventDefault();
+        inputTask.classList.add("alert");
+        inputTask.value = msg;
+        setTimeout(()=>{
+            inputTask.classList.remove("alert");
+            inputTask.value = "";
+        }
+        ,700)
+    }
+})
+let btns;
 function tasker(input){
     let task = document.createElement("div");
     task.classList.add("task");
@@ -17,8 +29,8 @@ function tasker(input){
     let btn = document.createElement("button");
     btn.setAttribute("type","submit");
     btn.classList.add("btn");
-    btn.innerText = "Delete"
+    btn.innerText = "Delete";
     task.appendChild(p);
     task.appendChild(btn);
-    taskContainer.appendChild(task);
+    taskContainer.prepend(task);
 }
