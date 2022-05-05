61e157a4f7c735593e4bcc19e2b12419f94cba29
rouanis5 <ouanis42@gmail.com>
2022-05-05 21:52:03 +0100

    big update

--- src/js/main.js
+++ src/js/main.js
@@ -1,111 +1,85 @@
-let addTask = document.getElementById("addTask");
-let inputTask = document.getElementById("inputTask");
-let autorization = false;
-let taskContainer = document.querySelector(".tasks .container");
-let tasks = [];
+let addBtn = document.getElementById("addBtn");
+let input = document.getElementById("input");
+let tasksContainer = document.querySelector(".tasks .container");
+let tasks = {};
+//create a task template --------------------------------------------
+function taskTemplate(id, title = "", isCompleted = false) {
+  let completeClass = isCompleted ? "completed" : "";
+  let completeTxt = isCompleted ? "undone" : "done";
+  return `
+    <div class='task ${completeClass}' data-id="${id}">
+        <p>${title}</p>
+        <button type="submit" class="deleteBtn" data-id="${id}">delete</button>
+        <button type="submit" class="completeBtn" data-id="${id}">${completeTxt} </button>
+    </div>
+  `;
+}
+//-------------------------------------------------------------------
+function displayTask(data) {
+  let clone = taskTemplate(data.id, data.title, data.completed);
+  tasksContainer.innerHTML += clone;
+}
 //initalizing local storage
 //the localStorage dont support arrays
 //for that I used JSON.parse and JSON stringify
 let tasksList = JSON.parse(localStorage.getItem("tasksList"));
-function setLocalStorage(){
-    localStorage.setItem("tasksList", JSON.stringify(tasks));
-    if (autorization) { // to check if the addTask btn is clicked or not
-        inputTask.value = ""; //delte the previous value
-        autorization = false;
-    }
+function setLocalStorage() {
+  localStorage.setItem("tasksList", JSON.stringify(tasks));
 }
-addTask.addEventListener("click",(e) =>{
-    let msg = "Please fill the chunck !";
-    autorization = true; //say that this btn is clicked
-    if (inputTask.value !== "" && inputTask.value !== msg){
-        const el = {                    //the task detail
-            id : Date.now(),
-            completed: false,
-            title: inputTask.value
-        }
-        tasks.push(el);
-        tasker(el);
-        setLocalStorage();
-    }
-    else{
-        e.preventDefault();
-        inputTask.classList.add("alert");
-        inputTask.value = msg;
-        setTimeout(()=>{
-            inputTask.classList.remove("alert");
-            inputTask.value = "";
-        }
-        ,700)
-    }
-})
-//create a task template --------------------------------------------
-let task = document.createElement("div");
-task.classList.add("task");
-let p = document.createElement("p"); //first child
-task.appendChild(p);
-
-let deleteBtn = document.createElement("button"); //2nd child
-deleteBtn.setAttribute("type","submit");
-deleteBtn.classList.add("deleteBtn");
-deleteBtn.innerText = "Delete";
-task.appendChild(deleteBtn);
-
-let completeBtn = document.createElement("button"); //last child
-completeBtn.setAttribute("type","submit");
-completeBtn.classList.add("completeBtn");
-completeBtn.innerText = "done";
-task.appendChild(completeBtn);
-
-//-------------------------------------------------------------------
-function tasker(data){
-    let clone = task.cloneNode(true);
-    clone.firstChild.innerText = data.title;
-    clone.setAttribute("data-id", data.id);
-    if (data.completed) {
-        clone.classList.add("completed");
-        clone.lastChild.innerText = "undone"
-    }
-    else{
-        clone.lastChild.innerText = "done"
-    }
-    taskContainer.prepend(clone);
-}
-
+//--------------------------------------------------------------------
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let msg = "Please fill the chunck !";
+  if (input.value !== "" && !input.classList.contains("alert")) {
+    const data = {
+      //the task detail
+      id: Date.now(),
+      completed: false,
+      title: input.value,
+    };
+    displayTask(data);
+    tasks.push(data);
+    setLocalStorage();
+  } else {
+    input.classList.add("alert");
+    input.value = msg;
+    setTimeout(() => {
+      input.classList.remove("alert");
+      input.value = "";
+    }, 700);
+  }
+});
 //get tasks from localStorage
-if (tasksList) {
-    tasks = tasksList;
-    for (let i = 0; i < tasks.length; i++) {
-        tasker(tasks[i]);
-    }
+if (tasks) {
+  tasks.forEach((task) => {
+    displayTask(task);
+  });
 }
-
 // js selector can't read DOM created after the script is runned
-// than we used the e.target to detect the button 
-taskContainer.addEventListener("click",(e)=>{
-    if (e.target.classList.contains("completeBtn")) {  //toggle complete 
-        e.target.parentElement.classList.toggle("completed");
-        for (let i = 0; i < tasks.length; i++) {
-            if (tasks[i].id ==  e.target.parentElement.getAttribute("data-id")) {
-                if (tasks[i].completed) {
-                    tasks[i].completed = false;
-                    e.target.innerText = "done"
-                }
-                else{
-                    tasks[i].completed = true;
-                    e.target.innerText = "undone"
-                }
-                setLocalStorage();
-                break;
-            }
-        }
-    }
-    else if (e.target.classList.contains("deleteBtn")) {
-        for (let i = 0; i < tasks.length; i++) {
-            if (tasks[i].id ==  e.target.parentElement.getAttribute("data-id")) {
-                tasks.splice(i, 1) // delete the task from the array
-                setLocalStorage();
-            }
-        }
-        e.target.parentElement.remove();
-    }
-})
+// than we used the e.target.matches to detect the button selector
+function addGlobalEventListener(event, selector, callback) {
+  document.addEventListener(event, (e) => {
+    if (e.target.matches(selector)) callback(e);
+  });
+}
+function getTaskId(id) {
+  tasks.forEach((task, index) => {
+    if (task.id === id) return index;
+  });
+}
+addGlobalEventListener("click", "button.completeBtn", (e) => {
+  let btn = e.target;
+  btn.parentElement.classList.toggle("completed");
+  let index = getTaskById(btn.getAttribute("data-id"));
+  [tasks[index].completed, btn.innerText] = tasks[index].completed
+    ? [false, "done"]
+    : [true, "undone"];
+  setLocalStorage();
+});
+addGlobalEventListener("click", "button.deleteBtn", (e) => {
+  let btn = e.target;
+  btn.parentElement.remove();
+  let index = getTaskId(btn.getAttribute("data-id"));
+  tasks.splice(index, 1); // delete the task from the array
+  setLocalStorage();
+});
